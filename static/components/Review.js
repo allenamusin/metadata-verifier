318f60fac5d63e6c7fb4cde2fd9f356439f9603b
Allen Amusin <allenamusin@gmail.com>
2019-12-07 14:19:16 -0800

    fixed spacing

--- static/components/Review.js
+++ static/components/Review.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
 export default class Review extends Component {
-  render() {
-    const {val, current} = this.props;
-    return (
-       <div >
-          <p> You are on the "Review" tab. Thanks for completing metadata verification. </p>
-       </div>
-    );
-  }
+    render() {
+        const {val, current} = this.props;
+        return (
+            <div >
+                <p> You are on the "Review" tab. Thanks for completing metadata verification. </p>
+            </div>
+        );
+    }
 }
--- static/components/TabContents.js
+++ static/components/TabContents.js
@@ -3,16 +3,16 @@ import Upload from './Upload';
 import Review from './Review';
 
 export default class TabContents extends Component {
-  render() {
-    const {val, current} = this.props;
-    const message = [
-      <Upload/>,
-      <Review/>,
-    ];
-    return (
-       <div >
-          {message[current]}
-       </div>
-    );
-  }
+    render() {
+        const {val, current} = this.props;
+        const message = [
+            <Upload/>,
+            <Review/>,
+        ];
+        return (
+            <div >
+                {message[current]}
+            </div>
+        );
+    }
 }
