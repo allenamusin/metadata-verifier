473bd4b6904370f484f46c3b75bddd9bed1b0b64
Allen Amusin <allenamusin@gmail.com>
2019-12-26 17:13:04 -0800

    Fix whitespace in Header.js

--- static/components/Header.js
+++ static/components/Header.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default class Header extends React.Component {
     render() {
-		    return (
-			      <div class="header logoWrapper">
-						<img class="logo" src='/static/logo.png' alt="logo" />
-						<div class="logOutText">
-						    <div>
-								    <p><a href="{% url 'account_logout' %}">Log Out</a></p>
-								</div>
+        return (
+            <div class="header logoWrapper">
+                <img class="logo" src='/static/logo.png' alt="logo" />
+                <div class="logOutText">
+                    <div>
+                        <p><a href="{% url 'account_logout' %}">Log Out</a></p>
+                    </div>
+                </div>
             </div>
-						</div>
         );
     }
 }
